d955baf2ae81566fa7926aa8201dfad82a0c192e
Michaela Ashlee <mmashlee@gmail.com>
2022-05-10 18:41:10 -0700

    Added userProfilePage.html and populated user information from the database.

---
+++ 2800 web /app.js
@@ -0,0 +1,227 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const req = require("express/lib/request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const port = 3000;
+
+const app = express();
+
+app.set("view engine", "html");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "password",
+  resave: false,
+  saveUninitialized: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/usersDB", { useNewUrlParser: true });
+
+const usersSchema = {
+  email: {
+    type: String,
+    required: [true, "enter your email"]
+  },
+  password: {
+    type: String,
+    required: [true, "enter your password"]
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  }
+};
+
+const Users = new mongoose.model("Users", usersSchema);
+
+const admin1 = new Users({
+  email: "eliyahabibi@gmail.com",
+  password: 123,
+  admin: true
+});
+
+const admin2 = new Users({
+  email: "michaela@gmail.com",
+  password: 123,
+  admin: true
+});
+
+const admin3 = new Users({
+  email: "liana@gmail.com",
+  password: 123,
+  admin: true
+});
+const admin4 = new Users({
+  email: "colin@gmail.com",
+  password: 123,
+  admin: true
+});
+
+// Users.insertMany([admin1, admin2, admin3, admin4], function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("saved successfully");
+//   }
+// });
+
+// Users.insertMany("/data.json", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("saved successfully");
+//   }
+// });
+
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/adminDash.html", function (req, res) {
+  res.sendFile(__dirname + "/adminDash.html");
+});
+
+app.get("/login.html", function (req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
+
+app.get("/signUp.html", function (req, res) {
+  res.sendFile(__dirname + "/signUp.html");
+});
+
+app.get("/adminDash.html", function (req, res) {
+  res.sendFile(__dirname + "/adminDash.html");
+});
+
+app.get("/index2.html", (req, res) => {
+  if (req.session.users) {
+    res.sendFile(__dirname + "/index2.html");
+  }
+  else {
+    res.redirect("/login.html");
+  }
+});
+
+
+app.get("/userProfilePage.html", function (req, res) {
+
+  if (req.session.loggedIn) {
+
+
+    let userProfilePage = fs.readFileSync(__dirname + "/userProfilePage.html", "utf8");
+    let changeToJSDOM = new JSDOM(userProfilePage);
+    
+    changeToJSDOM.window.document.getElementById("emailBox").placeholder = req.session.email;
+    changeToJSDOM.window.document.getElementById("userPassword").placeholder = req.session.password;
+
+    res.send(changeToJSDOM.serialize());
+
+  } else {
+    res.redirect("/login.html");
+  }
+
+});
+
+app.post("/userProfilePage.html", function (req, res){
+  
+  const currentEmail = req.session.email;
+
+  Users.updateOne({ email: currentEmail }, {$set: {"email": req.body.emailBox}})
+
+
+});
+
+    // const newUser = new Users({
+    //   email: req.body.emailBox,
+    //   password: req.body.userPassword,
+    //   isAdmin: false
+    // });
+  
+    // newUser.save(function (err) {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     res.sendFile(__dirname + "/index2.html");
+    //   }
+    // });
+
+
+
+app.post("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  req.session.destroy();
+  res.redirect(__dirname + "/");
+});
+
+app.post("/adminDash.html", function (req, res) {
+  Users.find(function (err, users) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(users);
+    }
+  });
+});
+
+app.post("/signUp.html", function (req, res) {
+  const newUser = new Users({
+    email: req.body.emailBox,
+    password: req.body.password,
+    isAdmin: false
+  });
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.sendFile(__dirname + "/index2.html");
+    }
+  });
+});
+
+
+app.post("/login.html", function (req, res) {
+  const username = req.body.emailBox;
+  const password = req.body.password;
+  const isAdmin = Users.admin;
+
+  Users.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser && foundUser.admin === false) {
+        if (foundUser.password === password) {
+          req.session.user = foundUser;
+          req.session.loggedIn = true;
+          req.session.email = username;
+          req.session.password = password;
+          res.sendFile(__dirname + "/index2.html");
+        }
+      }
+      if (foundUser && foundUser.admin === true) {
+        if (foundUser.password === password && foundUser.admin != isAdmin) {
+          req.session.users = foundUser;
+          req.session.loggedIn = true;
+          req.session.email = username;
+          req.session.password = password;
+          res.sendFile(__dirname + "/adminDash.html");
+        }
+      }
+    }
+  });
+});
+
+
+app.listen(port, function () {
+  console.log("server started on port " + port);
+});
---
+++ 2800 web /public/js/userProfilePage.js
@@ -0,0 +1,8 @@
+
+function edit() {
+  //Allows us to edit the form fields where the element is in personalInfo.html.
+  document.getElementById('personalInfoFields').disabled = false;
+}
+
+
+
