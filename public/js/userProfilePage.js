bb82f99773852906c75890a9c29fb2beceb9fc4e
Michaela Ashlee <mmashlee@gmail.com>
2022-05-13 04:42:36 -0700

    Completed the css styling for the user profile page in both desktop and mobile.

--- public/js/userProfilePage.js
+++ public/js/userProfilePage.js
@@ -1,6 +1,6 @@
 
 //The following code follows a demo represented by the instructor's in 1800 course Projects. 
-function edit() {
+function update() {
   //Allows us to edit the form fields where the element is in personalInfo.html.
   document.getElementById("userFields").disabled = false;
 }
