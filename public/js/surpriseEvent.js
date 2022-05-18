fe1b594093379e9bdbc90ea352d27642f8306233
ldiu <97375516+ldiu@users.noreply.github.com>
2022-05-18 00:05:38 -0700

    Created event to add background image on logo click

---
+++ public/js/surpriseEvent.js
@@ -0,0 +1,5 @@
+document.getElementById("logo").addEventListener("click", changeBackground);
+
+function changeBackground() {
+    document.body.style.backgroundImage = "url('img/MILCyway.jpg')";
+}
