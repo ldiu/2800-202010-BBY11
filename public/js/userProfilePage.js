9b3eb4a6abbd6db843a95737ca3c790dd99be9a6
ldiu <97375516+ldiu@users.noreply.github.com>
2022-05-19 16:37:24 -0700

    Update userProfilePage.js

--- public/js/userProfilePage.js
+++ public/js/userProfilePage.js
@@ -12,49 +12,6 @@ async function loadTimeline() {
       method: 'GET'
     });
     // if (posts.status === 200) {
-<<<<<<< Updated upstream
-      let timelinePosts = await posts.json();
-
-
-      let userPosts = document.createElement('div');
-      // parse through the timeline objects
-      for (var i = 0; i < timelinePosts.length; i++) {
-        let post = document.createElement('div');
-        post.className = "post";
-
-        //Create Header and add Date string from timeline and button
-        let headerDivElement = document.createElement('div');
-        let editButton = document.createElement('button');
-        headerDivElement.className = "postheader";
-        headerDivElement.innerHTML = timelinePosts[i].date;
-        headerDivElement.appendChild(editButton);
-        let timelineImages = timelinePosts[i].images;
-
-
-        //Create Image Section adapted from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
-        let imageContainerElement = document.createElement('div');
-        imageContainerElement.className = "imageContainer";
-        for (var x = 0; x < timelineImages.length; x++) {
-          let imageDivElement = document.createElement('div');
-          imageDivElement.className = "postImages";
-          imageDivElement.style = "";
-
-          let imageElement = document.createElement('img');
-          imageElement.src = timelineImages[x].path;
-          imageDivElement.appendChild(imageElement);
-          imageContainerElement.appendChild(imageDivElement);
-  
-        }
-
-        //Create Body Section and add text field from timeline
-        let bodyDivElement = document.createElement('div');
-        bodyDivElement.className = "postbody";
-        bodyDivElement.innerHTML = timelinePosts[i].text;
-
-        post.append(headerDivElement, imageContainerElement, bodyDivElement);
-
-        userPosts.append(post);
-=======
     let timelinePosts = await posts.json();
 
 
@@ -90,7 +47,6 @@ async function loadTimeline() {
         imageDivElement.appendChild(imageElement);
         imageContainerElement.appendChild(imageDivElement);
 
->>>>>>> Stashed changes
       }
 
       //Create Body Section and add text field from timeline
