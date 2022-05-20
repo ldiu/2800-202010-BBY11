c6bd323bb61a2b1128f00ab44f8b7dd4ab9f4df6
921coma <74879707+ColinLPH@users.noreply.github.com>
2022-05-20 11:33:22 -0700

    small fix: multer

--- app.js
+++ app.js
@@ -38,21 +38,6 @@ const imageStore = multer.diskStorage({
 
 const imageLoader = multer({ storage: imageStore });
 
-//from arrons notes
-const multer = require("multer");
-
-//Resource retrieved from Instructor Arron's 2537 example "upload-app.js"
-const imageStore = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "./public/img")
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.originalname.split('/').pop().trim());
-  }
-});
-
-const imageLoader = multer({ storage: imageStore });
-
 app.set("view engine", "html");
 app.use(express.static("public"));
 
